fb7fa70fd9b4ded858e3d020b316cbf515aa9d00
Biraj Kumar Sahu <biraj.sahu@haud.com>
2021-08-18 13:38:39 +0530

    add env change

--- src/test/java/automationHaud/AutomationHaudTest.java
+++ src/test/java/automationHaud/AutomationHaudTest.java
@@ -1,14 +1,11 @@
 package automationHaud;
 
-import com.intuit.karate.KarateOptions;
 import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
-import com.intuit.karate.junit5.Karate;
-
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
-@KarateOptions(tags = {"smpp_to_http"})
+
 class AutomationHaudTest {
 
     @Test
--- src/test/java/karate-config.js
+++ src/test/java/karate-config.js
@@ -2,11 +2,21 @@ function fn() {
     
   var configApplication = Java.type('config.configApplication');
   
-  var config = {
-    apiUrl: configApplication.getGlobalValue("apiUrl"),
-    sttUrl: configApplication.getGlobalValue("sttUrl"),
-    userName: configApplication.getGlobalValue("userName"),
-    password: configApplication.getGlobalValue("password")
+  var env = karate.env; // get system property 'karate.env'    vs
+  karate.log('karate.env system property was:', env);
+
+  if (!env) {
+    env = 'dev';
   }
+  
+  var config = { };
+  
+  if (env == 'dev') {
+    config.apiUrl = configApplication.getGlobalValue("apiUrl");
+    config.sttUrl = configApplication.getGlobalValue("sttUrl");
+    config.userName = configApplication.getGlobalValue("userName");
+    config.password = configApplication.getGlobalValue("password");
+  }
+
   return config;
 }
